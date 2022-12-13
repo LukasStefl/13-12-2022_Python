f5e45b1ea02e25d62329387f5902d8d9991e7e90
LukasStefl <lukas.stefl@student.ossp.cz>
2022-12-13 10:13:07 +0100

    commit

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 import os
-f = open("names.txt", "w")
-f.write("Now the file has more content!")
-f.close()
-
-#open and read the file after the appending:
 f = open("names.txt", "r")
+
 print(f.read())
-#os.remove("demofile2.txt")
